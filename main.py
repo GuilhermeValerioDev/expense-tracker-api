e319ab7105480c13ff338173f8c8d49526b9b8ed
Guilherme Valerio <guilhermevprofissional@gmail.com>
2026-06-27 15:53:57 -0300

    Added get/users

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 from fastapi import FastAPI
+from fastapi import Depends
+from sqlalchemy.orm import Session
+from sqlalchemy import select
+from database import get_db
+from models import User
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"message": "Hello World!"}
+
+
+@app.get("/users")
+def get_users(db: Session = Depends(get_db)):
+    result = db.execute(select(User))
+    users = result.scalars().all()
+    return users
+
