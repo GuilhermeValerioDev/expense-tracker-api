aeeebc6349d5cefb5158b5a143cff7c7c6099913
Guilherme Valerio <guilhermevprofissional@gmail.com>
2026-06-29 17:03:45 -0300

    Implement POST /users and response schemas

--- main.py
+++ main.py
@@ -4,6 +4,12 @@ from sqlalchemy.orm import Session
 from sqlalchemy import select
 from database import get_db
 from models import User
+from schemas import UserCreate, UserResponse
+
+# podman container start expense-db
+# podman exec -it expense-db psql -U bism -d expense_tracker
+
+# uvicorn main:app --reload
 
 app = FastAPI()
 
@@ -12,9 +18,21 @@ def read_root():
     return {"message": "Hello World!"}
 
 
-@app.get("/users")
+@app.get("/users", response_model=list[UserResponse])
 def get_users(db: Session = Depends(get_db)):
     result = db.execute(select(User))
     users = result.scalars().all()
     return users
 
+
+@app.post("/users", response_model=UserResponse)
+def create_user(user: UserCreate, db: Session = Depends(get_db)):
+    new_user = User(
+        username = user.username,
+        email = user.email,
+        password_hash = user.password
+    )
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
